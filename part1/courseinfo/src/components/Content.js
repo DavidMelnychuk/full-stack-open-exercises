0e117884f8ea407fa48c3d42a71ce30cf3baf9a1
David Melnychuk <davidm9779@gmail.com>
2020-03-02 22:41:13 -0800

    complete 2.1

---
+++ part1/courseinfo/src/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Part from './Part'
+
+const Content = ({ parts }) => {
+  const rows = () => parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>);
+  
+  return (
+    <div> 
+        {rows()}
+    </div>
+  );
+};
+
+export default Content;
