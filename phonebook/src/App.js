169dabbc223c0c808f9e688802d10ce8815cdc71
David Melnychuk <davidm9779@gmail.com>
2020-03-05 17:45:05 -0800

    add fetching with axios library

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,38 +1,63 @@
-import React, { useState } from "react";
-import Person from './components/Person'
+import React, { useState, useEffect } from "react";
+import Person from "./components/Person";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const hook = () => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const handleNameChange = event => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value);
+  };
+
   const addPerson = event => {
     event.preventDefault();
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
     setPersons(persons.concat(newPerson));
     setNewName("");
   };
 
-  const displayNumbers = () => persons.map(person => <Person key={person.name} name={person.name}/>)
+  const displayNumbers = () =>
+    persons.map(person => (
+      <Person key={person.name} name={person.name} number={person.number} />
+    ));
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <h1>Phonebook</h1>
+      <h2>Add a New Person</h2>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <div>{displayNumbers()}</div>
-      <div>debug: {newName}</div>
+      <Persons displayNumbers={displayNumbers} />
     </div>
   );
 };
