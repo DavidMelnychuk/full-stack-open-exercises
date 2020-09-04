296af7f96879764b0fb35b0c4d8582cfb11517e0
David Melnychuk <davidm9779@gmail.com>
2020-09-03 19:46:14 -0700

    add test for POST /api/blogs

--- blog/tests/blog_api.test.js
+++ blog/tests/blog_api.test.js
@@ -3,14 +3,15 @@ const supertest = require("supertest");
 const app = require("../app");
 const helper = require("./test_helper");
 const Blog = require("../models/blog");
+const { initialBlogs } = require("./test_helper");
 
 beforeEach(async () => {
   // Delete and intialize blog database before each test.
   await Blog.deleteMany({});
 
-  helper.initialBlogs.forEach(async (blog) => {
-    await new Blog(blog).save();
-  });
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
 });
 
 const api = supertest(app);
@@ -18,6 +19,28 @@ const api = supertest(app);
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
   expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "Random",
+    author: "boogey",
+    url: "google.ca",
+    likes: 5,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-type", "application/json; charset=utf-8");
+
+  const response = await api.get("/api/blogs");
+
+  const titles = response.body.map((r) => r.title);
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+  expect(titles).toContain("Random");
 });
 
 afterAll(() => {
