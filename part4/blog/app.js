e26b19881b565dc414a4d8a770297a8d893306c2
David Melnychuk <davidm9779@gmail.com>
2020-09-04 16:42:00 -0700

    add creating user

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const config = require("./utils/config");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 
@@ -25,6 +26,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
